d511a92a33fdbf468e2146cc4ebd691117b58f82
Andre Petrucci <ace.petrucci@outlook.com>
2023-04-12 15:24:45 -0300

    chore: add learner-ui package

---
+++ packages/learner-ui/src/components/Choice/Choice.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Choice as ChoiceComponent } from "./Choice";
+
+const meta = {
+  component: ChoiceComponent,
+  title: "Learner/Choice",
+} satisfies Meta<typeof ChoiceComponent>;
+
+type Story = StoryObj<typeof meta>;
+
+export const Choice = {
+  args: {},
+} satisfies Story;
+
+export default meta;

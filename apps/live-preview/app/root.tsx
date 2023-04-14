77a0aae00266e1872f01336aae11125a8aaa8b41
Tim Johnson <jhnsntmthy@gmail.com>
2023-04-14 16:42:13 -0400

    remove scroll-blocking body classes

--- apps/live-preview/app/root.tsx
+++ apps/live-preview/app/root.tsx
@@ -31,7 +31,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="flex pb-20">
+      <body>
         <Outlet />
 
         <ScrollRestoration />

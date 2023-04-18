e969a042546d9c22e83ff22e31f7c1041849d71e
LeathanTeal <leathan@gmail.com>
2023-04-18 15:39:22 -0700

    Feedback score

--- packages/learner-ui/src/components/MultipleChoice/components/MultipleChoiceFeedback/MultipleChoiceFeedback.tsx
+++ packages/learner-ui/src/components/MultipleChoice/components/MultipleChoiceFeedback/MultipleChoiceFeedback.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Doughnut } from "react-chartjs-2";
 
+import { ArcElement, Chart as ChartJS } from "chart.js";
 import { AnimatePresence, motion } from "framer-motion";
 
 import { GreenCheckMedallion } from "~/components/images/GreenCheckMedallion";
 import { RedCircleX } from "~/components/images/RedCircleX";
 
 import type { MultipleChoiceFeedbackProps } from "./MultipleChoiceFeedback.types";
+
+ChartJS.register(ArcElement);
 
 export const MultipleChoiceFeedback = ({
   feedback,
@@ -58,6 +62,36 @@ export const MultipleChoiceFeedback = ({
             <p>
               Total Score: <strong className="text-lime-700">{1200}</strong>
             </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex-col">
+              <p className="text-sm font-bold">Mastery:</p>
+              <p className="text-lg">Access Power</p>
+            </div>
+            <div className="relative h-32 w-32">
+              <Doughnut
+                options={{ cutout: "60%" }}
+                data={{
+                  datasets: [
+                    {
+                      data: [77, 23],
+                      backgroundColor: [
+                        "rgba(229, 229, 229, 1)",
+                        "rgba(229, 229, 229, 0)",
+                      ],
+                      borderColor: [
+                        "rgba(229, 229, 229, 1)",
+                        "rgba(229, 229, 229, 0)",
+                      ],
+                      borderWidth: 0,
+                    },
+                  ],
+                }}
+              />
+              <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl font-bold text-neutral-400">
+                75%
+              </p>
+            </div>
           </div>
         </motion.div>
       )}

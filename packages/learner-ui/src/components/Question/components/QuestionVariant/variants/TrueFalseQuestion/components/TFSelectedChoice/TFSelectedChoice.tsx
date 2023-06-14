6f1d1d4fad5da32ee57f46e7343bc934dc763580
Andre Petrucci <ace.petrucci@outlook.com>
2023-06-14 17:09:29 -0300

    chore: remove unneded opt chaining

--- packages/learner-ui/src/components/Question/components/QuestionVariant/variants/TrueFalseQuestion/components/TFSelectedChoice/TFSelectedChoice.tsx
+++ packages/learner-ui/src/components/Question/components/QuestionVariant/variants/TrueFalseQuestion/components/TFSelectedChoice/TFSelectedChoice.tsx
@@ -10,7 +10,7 @@ export const TFSelectedChoice: FC<TFSelectedChoiceProps> = ({
   selected,
   tfquestion,
 }) => {
-  const selectedChoice = selected && Boolean((JSON.parse(selected?.value)));
+  const selectedChoice = selected && Boolean(JSON.parse(selected.value));
 
   return (
     <AnimatePresence>

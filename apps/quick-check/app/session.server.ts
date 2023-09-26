8961c176bae45f39e19dd14a8c62434f60fc4358
Andre Petrucci <ace.petrucci@outlook.com>
2023-09-26 12:54:16 -0300

    feat: add now cookie to admin session

--- apps/quick-check/app/session.server.ts
+++ apps/quick-check/app/session.server.ts
@@ -1,6 +1,8 @@
 import { createCookieSessionStorage, redirect } from "@remix-run/node";
 
+import { DateTime } from "luxon";
 import invariant from "tiny-invariant";
+
 import type { User } from "~/graphql";
 
 import { SESSION_SECRET } from "~/utils/envs.server";
@@ -9,14 +11,18 @@ import { getUserFromRequest } from "~/models/user";
 
 invariant(SESSION_SECRET, "SESSION_SECRET must be set");
 
-type SessionData = {
-  userId: string;
-  tenantId: string;
-};
+/**
+ * Session Definition
+ */
 
-type SessionFlashData = {
-  error: string;
-};
+enum SessionKeys {
+  USER_ID = "userId",
+  TENANT_ID = "tenantId",
+  NOW = "now",
+}
+
+type SessionData = Record<SessionKeys, string>;
+type SessionFlashData = { error: string };
 
 export const sessionStorage = createCookieSessionStorage<
   SessionData,
@@ -32,20 +38,47 @@ export const sessionStorage = createCookieSessionStorage<
   },
 });
 
-const USER_ID_SESSION_KEY = "userId";
-const TENANT_ID_SESSION_KEY = "tenantId";
+/**
+ * Session Utils
+ */
+
+type Session = {
+  request: Request;
+  remember: boolean;
+  redirectTo: string;
+};
+
+type UserSession = Session & SessionData;
+type AdminSession = Session & Partial<Pick<SessionData, SessionKeys.NOW>>;
 
 export async function getSession(request: Request) {
   const cookie = request.headers.get("Cookie");
+
   return sessionStorage.getSession(cookie);
 }
+
+export async function logout(request: Request) {
+  const session = await getSession(request);
+
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await sessionStorage.destroySession(session),
+    },
+  });
+}
+
+/**
+ * User Session
+ */
 
 export async function getUserDataFromFromSession(
   request: Request,
 ): Promise<[User["user_id"] | undefined, User["tenant_id"] | undefined]> {
   const session = await getSession(request);
-  const userId = session.get(USER_ID_SESSION_KEY);
-  const tenantId = session.get(TENANT_ID_SESSION_KEY);
+
+  const userId = session.get(SessionKeys.USER_ID);
+  const tenantId = session.get(SessionKeys.TENANT_ID);
+
   return [userId, tenantId];
 }
 
@@ -54,10 +87,13 @@ export async function requireUserSession(
   redirectTo: string = new URL(request.url).pathname,
 ): Promise<[string, string]> {
   const [userId, tenantId] = await getUserDataFromFromSession(request);
+
   if (!userId || !tenantId) {
     const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+
     throw redirect(`/?${searchParams.toString()}`);
   }
+
   return [userId, tenantId];
 }
 
@@ -76,17 +112,11 @@ export async function createUserSession({
   tenantId,
   remember,
   redirectTo,
-}: {
-  request: Request;
-  userId: string;
-  tenantId: string;
-  remember: boolean;
-  redirectTo: string;
-}) {
+}: UserSession) {
   const session = await getSession(request);
 
-  session.set(USER_ID_SESSION_KEY, userId);
-  session.set(TENANT_ID_SESSION_KEY, tenantId);
+  session.set(SessionKeys.USER_ID, userId);
+  session.set(SessionKeys.TENANT_ID, tenantId);
 
   return redirect(redirectTo, {
     headers: {
@@ -99,11 +129,41 @@ export async function createUserSession({
   });
 }
 
-export async function logout(request: Request) {
+/**
+ * Admin Session
+ */
+
+export async function getAdminDataFromFromSession(request: Request) {
   const session = await getSession(request);
-  return redirect("/", {
+
+  const now = session.get(SessionKeys.NOW) ?? DateTime.now().toISO()!;
+
+  return now;
+}
+
+export async function requireAdminSession(request: Request): Promise<[string]> {
+  const now = await getAdminDataFromFromSession(request);
+
+  return [now];
+}
+
+export async function createAdminSession({
+  request,
+  remember,
+  redirectTo,
+  now,
+}: AdminSession) {
+  const session = await getSession(request);
+
+  session.set(SessionKeys.NOW, now ?? "");
+
+  return redirect(redirectTo, {
     headers: {
-      "Set-Cookie": await sessionStorage.destroySession(session),
+      "Set-Cookie": await sessionStorage.commitSession(session, {
+        maxAge: remember
+          ? 60 * 60 * 24 // 1 day
+          : undefined,
+      }),
     },
   });
 }

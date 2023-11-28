dadce927f01cad7a95ba8c9020fd589d9b63e2aa
André Petrucci <ace.petrucci@outlook.com>
2023-11-28 11:13:33 -0300

    Fix scroll (#130)

--- apps/quick-check/app/root.tsx
+++ apps/quick-check/app/root.tsx
@@ -104,7 +104,7 @@ export default function App() {
   useChangeLanguage(locale);
 
   return (
-    <html lang={locale} dir={i18n.dir()} className="h-full overflow-hidden">
+    <html lang={locale} dir={i18n.dir()} className="h-full">
       <head>
         <Meta />
         <Links />

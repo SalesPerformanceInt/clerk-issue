8e7264f2544758842d19149b8abefd1c8d61f368
Timothy Johnson <jhnsntmthy@gmail.com>
2024-01-02 11:47:32 -0500

    cleanup

--- tests/behaviour/cucumber.conf.js
+++ tests/behaviour/cucumber.conf.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 require("dotenv").config();
 const {
   Before,
@@ -7,11 +6,11 @@ const {
   After,
   setDefaultTimeout,
 } = require("@cucumber/cucumber");
-// you can choose other browsers like webkit or firefox according to your requirement
 const { chromium } = require("playwright");
+
 setDefaultTimeout(60000);
 const isLocal = process.env.CUKE_BASE_URL?.includes("localhost");
-// launch the browser
+
 BeforeAll(async function () {
   global.browser = await chromium.launch({
     headless: isLocal ? false : true,
@@ -19,18 +18,15 @@ BeforeAll(async function () {
   });
 });
 
-// close the browser
 AfterAll(async function () {
   await global.browser.close();
 });
 
-// Create a new browser context and page per scenario
 Before(async function () {
   global.context = await global.browser.newContext();
   global.page = await global.context.newPage();
 });
 
-// Cleanup after each scenario
 After(async function () {
   await global.page.close();
   await global.context.close();

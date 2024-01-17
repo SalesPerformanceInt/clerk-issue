6fb41c08784f123ae3e243a84b09b7fa4d5711f2
Ramkrishan Patidar <ram@gemsessence.com>
2024-01-17 19:03:33 +0530

    VIB-9034: allow cors for api/user/token

--- apps/quick-check/app/routes/api.user.token.ts
+++ apps/quick-check/app/routes/api.user.token.ts
@@ -1,10 +1,13 @@
-import { json, type ActionFunctionArgs } from "@remix-run/node";
-
+import { json, type ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { cors } from "remix-utils/cors";
 import { invariant, simpleErrorResponse } from "quickcheck-shared";
 
 import { getAdminApolloClientFromRequest } from "~/graphql";
 
 import { getLoginUrl } from "~/utils/urls";
+import {
+  ALLOWED_ORIGIN_REGEX,
+} from "~/utils/envs.server";
 
 import { getUserSchema, verifyApiRequest } from "~/models/api";
 import { getUserActiveToken } from "~/models/token";
@@ -12,6 +15,15 @@ import { getUserActiveToken } from "~/models/token";
 export const config = {
   maxDuration: 300,
 };
+
+// Browser will send the options request so we need to handle options request with allow cors origin header.
+// Just returning null with 204 status.
+export async function loader({ request }: LoaderFunctionArgs) {
+	let response = json(null, { status: 204 });
+  // this mutates the Response object
+	await cors(request, response, { origin: new RegExp(ALLOWED_ORIGIN_REGEX) });
+	return response;
+}
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   try {
@@ -38,7 +50,10 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       questionsAvailable: 0, //TODO: find the user's unanswered question count
     };
 
-    return json(response);
+    const jsonResponse = json(response);
+    // this mutates the jsonResponse object
+    await cors(request, jsonResponse, { origin: new RegExp(ALLOWED_ORIGIN_REGEX) });
+    return jsonResponse
   } catch (error) {
     return simpleErrorResponse(error);
   }

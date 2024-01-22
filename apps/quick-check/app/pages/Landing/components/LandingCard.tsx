72e3693eab771660824afba2b609ae17a387d835
Leathan Teal <leathan@gmail.com>
2024-01-22 09:39:29 -0800

    Send email  and landing state on clicking expired link (#187)
    * Send email on expired link
    
    * Remove unreachable invariant

--- apps/quick-check/app/pages/Landing/components/LandingCard.tsx
+++ apps/quick-check/app/pages/Landing/components/LandingCard.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { useSearchParams } from "@remix-run/react";
 
 import { useFormContext } from "remix-validated-form";
 
@@ -7,14 +8,20 @@ import { Card } from "quickcheck-shared";
 
 import { LandingEmailSubmit } from "./LandingEmailSubmit";
 import { LandingEmailSubmitted } from "./LandingEmailSubmitted";
+import { LandingExpiredLink } from "./LandingExpiredLink";
 
 export const LandingCard: FC = () => {
   const { t } = useTranslation();
   const { hasBeenSubmitted } = useFormContext();
+  const [searchParams] = useSearchParams();
 
-  return (
-    <Card className="my-6 p-8 lg:my-8">
-      {hasBeenSubmitted ? <LandingEmailSubmitted /> : <LandingEmailSubmit />}
-    </Card>
-  );
+  const expired = searchParams.get("expired") === "true";
+
+  const renderContent = () => {
+    if (expired) return <LandingExpiredLink />;
+    if (hasBeenSubmitted) return <LandingEmailSubmitted />;
+    return <LandingEmailSubmit />;
+  };
+
+  return <Card className="my-6 p-8 lg:my-8">{renderContent()}</Card>;
 };

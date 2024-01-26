b00e023017a9df99591d724c3723c403d6b9d779
Timothy Johnson <jhnsntmthy@gmail.com>
2024-01-26 15:28:38 -0500

    Added my modified invariant for better logging (#194)

--- packages/quickcheck-shared/src/utils/invariant.ts
+++ packages/quickcheck-shared/src/utils/invariant.ts
@@ -1,10 +1,29 @@
 // Based on [tiny-invariant](https://www.npmjs.com/package/tiny-invariant)
+import { logError } from "./logger";
 
 const PREFIX = "Invariant failed";
+
+export class InvariantError extends Error {
+  constructor(args: any) {
+    super(args);
+    this.name = "InvariantError";
+    logError({ error: args, log: this.name });
+  }
+}
+Object.defineProperty(InvariantError.prototype, "toJSON", {
+  value: function () {
+    return { invariant: this.message };
+  },
+  configurable: true,
+  writable: true,
+});
+
+export type AnyError = Error | InvariantError;
 
 export function invariant(
   condition: any,
   message?: string | (() => string),
+  loggable?: any,
 ): asserts condition {
   if (condition) {
     return;
@@ -12,9 +31,10 @@ export function invariant(
 
   const provided = typeof message === "function" ? message() : message;
 
-  const value = provided 
-    ? `${PREFIX}: ${provided}, got: ${condition}` 
-    : PREFIX;
-    
-  throw new Error(value);
+  const value = provided ? `${PREFIX}: ${provided}, got: ${condition}` : PREFIX;
+  console.error({
+    unexpected: loggable,
+    json: JSON.stringify(loggable, null, 2),
+  });
+  throw new InvariantError(value);
 }

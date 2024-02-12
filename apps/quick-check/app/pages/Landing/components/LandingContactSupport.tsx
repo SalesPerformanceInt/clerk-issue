35789e9e83575b9ff6cd554e3f0072e622134ffd
Leathan Teal <leathan@gmail.com>
2024-02-12 14:29:13 -0800

    Fix contact support mailto link (#204)
    * mailto
    
    * LinkButton

--- apps/quick-check/app/pages/Landing/components/LandingContactSupport.tsx
+++ apps/quick-check/app/pages/Landing/components/LandingContactSupport.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
 
-import { Button } from "quickcheck-shared";
+import { LinkButton } from "quickcheck-shared";
 
 export const LandingContactSupport: FC = () => {
   const { t } = useTranslation();
@@ -9,16 +9,15 @@ export const LandingContactSupport: FC = () => {
   return (
     <div className="flex items-center justify-between">
       <p className="">{t("landing.no_account")}</p>
-      <Button
+      <LinkButton
+        href="mailto:support@richardsonaccelerate.com"
+        target="_blank"
         background="light"
         variant="secondary"
         className="w-auto min-w-44"
-        onClick={() => {
-          window.open("support@richardsonaccelerate.com", "_blank");
-        }}
       >
         {t("common.contact_support")}
-      </Button>
+      </LinkButton>
     </div>
   );
 };

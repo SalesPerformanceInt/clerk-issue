135001244768c17ea8f455b7cb29215fcf006d51
LeathanTeal <leathan@gmail.com>
2024-03-20 11:18:44 -0700

    Pagination

--- apps/quick-check/app/components/Pagination/Pagination.tsx
+++ apps/quick-check/app/components/Pagination/Pagination.tsx
@@ -1,7 +1,25 @@
-import React, { type FC } from "react";
+import { useState, type FC } from "react";
 import ReactPaginate, { ReactPaginateProps } from "react-paginate";
 
 import { twMerge } from "tailwind-merge";
+
+export const usePagination = <T,>(items: T[], itemsPerPage = 10) => {
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  const onPageChange: PaginationProps["onPageChange"] = ({ selected }) => {
+    const newOffset = (selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+  };
+
+  return {
+    onPageChange,
+    currentItems,
+    pageCount,
+  };
+};
 
 export type PaginationProps = Pick<
   ReactPaginateProps,
@@ -10,7 +28,6 @@ export type PaginationProps = Pick<
 
 const pageClassName =
   "flex items-center justify-center px-6 py-4 text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700";
-
 const linkClassName = "text-primary-50 -m-4 p-4";
 
 export const Pagination: FC<PaginationProps> = ({
@@ -23,6 +40,8 @@ export const Pagination: FC<PaginationProps> = ({
       nextLabel=">>"
       onPageChange={onPageChange}
       pageCount={pageCount}
+      // pageRangeDisplayed={3}
+      // marginPagesDisplayed={2}
       previousLabel="<<"
       renderOnZeroPageCount={null}
       containerClassName="inline-flex -space-x-px text-sm"
--- apps/quick-check/app/routes/content.$language.courses.tsx
+++ apps/quick-check/app/routes/content.$language.courses.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
 
 import { getContentStackClient } from "~/contentstack.server";
-import { first, isArray } from "remeda";
+import { first, isArray, sortBy } from "remeda";
 
 import { invariant, simpleErrorResponse } from "quickcheck-shared";
 
-import { Pagination, PaginationProps } from "~/components";
+import { Pagination, PaginationProps, usePagination } from "~/components";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   try {
@@ -35,23 +34,19 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
 export default function QuestionPage() {
   const { courses, language } = useLoaderData<typeof loader>();
 
-  const itemsPerPage = 10;
+  const orderedCorses = sortBy(courses, (c) => c.metadata.display_title);
 
-  const [itemOffset, setItemOffset] = useState(0);
-  const endOffset = itemOffset + itemsPerPage;
-  const currentCourses = courses.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(courses.length / itemsPerPage);
-
-  const handlePageClick: PaginationProps["onPageChange"] = ({ selected }) => {
-    const newOffset = (selected * itemsPerPage) % courses.length;
-    setItemOffset(newOffset);
-  };
+  const { onPageChange, currentItems, pageCount } =
+    usePagination(orderedCorses);
 
   return (
     <div className="sm:p-8">
       <div className="flex w-full flex-col">
         <div className="overflow-x-auto sm:-mx-6 desktop:-mx-8">
           <div className="inline-block min-w-full sm:px-6 desktop:px-8">
+            <div className="mb-8 flex items-center justify-center px-4 pt-4 sm:p-0">
+              <h1 className="text-center text-4xl font-bold">Courses</h1>
+            </div>
             <div className="overflow-hidden">
               <table className="min-w-full table-auto text-left text-sm">
                 <thead className="border-b bg-white font-medium">
@@ -66,7 +61,7 @@ export default function QuestionPage() {
                   </tr>
                 </thead>
                 <tbody>
-                  {currentCourses.map((course, row) => (
+                  {currentItems.map((course, row) => (
                     <tr
                       key={course.uid}
                       className={`border-b ${
@@ -90,10 +85,7 @@ export default function QuestionPage() {
                   ))}
                 </tbody>
               </table>
-              <Pagination
-                onPageChange={handlePageClick}
-                pageCount={pageCount}
-              />
+              <Pagination onPageChange={onPageChange} pageCount={pageCount} />
             </div>
           </div>
         </div>
